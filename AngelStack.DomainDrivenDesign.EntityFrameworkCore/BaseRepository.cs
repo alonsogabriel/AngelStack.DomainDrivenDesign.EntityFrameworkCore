92cbbcf8bfd6271b8189dad10b68e71237a89bdc
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2025-08-16 00:10:42 -0300

    Initial structure

---
+++ AngelStack.DomainDrivenDesign.EntityFrameworkCore/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using AngelStack.DomainDrivenDesign.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngelStack.DomainDrivenDesign.EntityFrameworkCore;
+
+public class BaseRepository<TContext, TEntity>(TContext context) : IRepository<TEntity>
+    where TContext : DbContext
+    where TEntity : class
+{
+    protected readonly TContext _context = context;
+
+    protected DbSet<TEntity> Entities { get; } = context.Set<TEntity>();
+
+    public async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await Entities.AddAsync(entity, cancellationToken);
+    }
+
+    public async Task<TEntity?> FindAsync(object id, CancellationToken cancellationToken)
+    {
+        return await Entities.FindAsync([id], cancellationToken);
+    }
+
+    public IQueryable<TEntity> Query()
+    {
+        return Entities.AsQueryable();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        Entities.Remove(entity);
+    }
+
+    public void Update(TEntity entity)
+    {
+        Entities.Update(entity);
+    }
+}
---
+++ AngelStack.DomainDrivenDesign.EntityFrameworkCore/Extensions.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using AngelStack.DomainDrivenDesign.Abstractions;
+
+namespace DomainDrivenDesign.Abstractions.EntityFrameworkCore;
+
+public static partial class Extensions
+{
+    public static PropertyBuilder<K> Property<T, K>(
+        this EntityTypeBuilder<T> builder,
+        Expression<Func<T, K>> property,
+        string? columnName)
+        where T : class
+    {
+        return builder.Property<K>(property).HasColumnName(columnName);
+    }
+
+    [GeneratedRegex(@"[A-Z]?[a-z0-9]+|[A-Z]+(?![a-z])", RegexOptions.Compiled)]
+    public static partial Regex PascalCaseRegex();
+
+    public static PropertyBuilder<K> SnakeCase<T, K>(
+        this EntityTypeBuilder<T> builder,
+        Expression<Func<T, K>> property)
+        where T : class
+    {
+        string? column = null;
+
+        if (property.Body is MemberExpression member)
+        {
+            var matches = PascalCaseRegex().Matches(member.Member.Name);
+            var parts = matches.Select(match => match.Value.ToLower());
+            column = string.Join('_', parts);
+        }
+
+        return builder.Property<K>(property).HasColumnName(column);
+    }
+
+    public static void MapBaseEntity<T, K>(
+        this EntityTypeBuilder<T> builder,
+        string? tableName = null,
+        string? schema = null,
+        bool snakeCaseColumns = false) where T : AbstractEntity<K>
+    {
+        (string? idColumn, string? createdAtColumn, string? updatedAtColumn) =
+            snakeCaseColumns ? ("id", "created_at", "updated_at") : (null, null, null);
+
+        if (tableName != null)
+        {
+            builder.ToTable(tableName, schema);
+        }
+
+        builder.HasKey(e => e.Id);
+
+        if (typeof(K) == typeof(Guid))
+        {
+            builder.Property(e => e.Id, idColumn).ValueGeneratedNever();
+        }
+
+        builder.Property(e => e.CreatedAt, createdAtColumn).IsRequired();
+        builder.Property(e => e.UpdatedAt, updatedAtColumn).IsRequired(false);
+    }
+
+    public static void MapBaseEntity<T>(
+        this EntityTypeBuilder<T> builder,
+        string? tableName = null,
+        string? schema = null) where T : AbstractEntity
+    {
+        builder.MapBaseEntity<T, Guid>(tableName, schema);
+    }
+
+    public static void MapSoftDelete<T>(
+        this EntityTypeBuilder<T> builder,
+        bool applyFilter = true) where T : class, ISoftDelete
+    {
+        builder.Property(e => e.DeletedAt).IsRequired(false);
+        if (applyFilter)
+        {
+            builder.HasQueryFilter(e => e.DeletedAt == null);
+        }
+    }
+
+    public static ModelBuilder MapEnum<T>(
+        this ModelBuilder modelBuilder,
+        string? tableName = null,
+        string? schema = null,
+        bool snakeCaseColumns = false) where T : AbstractEnum<T>
+    {
+        var builder = modelBuilder.Entity<T>();
+
+        if (tableName != null)
+        {
+            builder.ToTable(tableName, schema);
+        }
+
+        (string? valueColumn, string? nameColumn) =
+            snakeCaseColumns ? ("value", "name") : (null, null);
+
+        builder.HasKey(e => e.Value);
+        builder.Property(e => e.Value, valueColumn);
+        builder.Property(e => e.Name, nameColumn).IsRequired().HasMaxLength(50);
+        builder.HasIndex(e => e.Name).IsUnique();
+        builder.HasData(AbstractEnum<T>.GetValues());
+
+        return modelBuilder;
+    }
+
+    public static void MapEnumConversion<T>(this PropertyBuilder<T> builder)
+        where T : struct, Enum
+    {
+        builder.HasConversion(v => v.ToString(), v => Enum.Parse<T>(v));
+    }
+
+    public static void MapEnumConversion<T>(this PropertyBuilder<T?> builder)
+        where T : struct, Enum
+    {
+        builder.HasConversion(
+            v => v != null ? v.ToString() : null,
+            v => v != null ? Enum.Parse<T>(v) : null);
+    }
+
+    public static ReferenceCollectionBuilder<T, K> PreventDelete<T, K>(
+        this ReferenceCollectionBuilder<T, K> builer) where T : class where K : class
+    {
+        return builer.OnDelete(DeleteBehavior.NoAction);
+    }
+}
