a0ee49b5bc6dd0af7378b7e729c8e00a384dca78
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2025-08-31 14:47:11 -0300

    Fix json seed files configuration (#3)

---
+++ ConsoleTests/Migrations/20250831171911_RemovePhoneAreaCode.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleTests.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePhoneAreaCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneAreaCode",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "User",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneAreaCode",
+                table: "User",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+        }
+    }
+}
